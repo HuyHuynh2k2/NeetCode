379eef5ba85836e4ddb79a11fe73181e6a1b77a8
huyhuynh <huynhlonghuy.work@gmail.com>
2025-04-05 16:23:16 -0700

    Done

--- Arrays & Hashing/ProductOfArrayExceptItSelf.js
+++ Arrays & Hashing/ProductOfArrayExceptItSelf.js
@@ -10,24 +10,40 @@
  */
 
 const productExceptSelf = (nums) => {
-  let prefix = [nums[0]];
+  let prefix = [];
   let postfix = [];
-  for (let k = 0; k < nums.length - 2; k++) {
-    postfix[k] = 0;
-  }
-  postfix[nums.length - 1] = [nums[nums.length - 1]];
 
-  for (let i = 1; i < nums.length; i++) { 
-    prefix[i] = prefix[i - 1] * nums[i];
-  }
-
-  for (let j = nums.length - 2; j >= 0; j--) {
-    postfix[j] = postfix[j + 1] * nums[j];
+  for (let i = 0; i < nums.length; i++) {
+    if (i === 0) {
+      prefix[0] = nums[0];
+    } else {
+      prefix[i] = prefix[i - 1] * nums[i];
+    }
   }
 
-  console.log(postfix);
-  console.log(prefix);
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (i === nums.length - 1) {
+      postfix[i] = nums[nums.length - 1];
+    } else {
+      postfix[i] = postfix[i + 1] * nums[i];
+    }
+  }
+
+  let result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i === 0) {
+      result[i] = postfix[i + 1];
+    } else if (i === nums.length - 1) {
+      result[i] = prefix[i - 1];
+    } else {
+      result[i] = prefix[i - 1] * postfix[i + 1];
+    }
+  }
+
+  return result;
 };
 
 let nums = [1, 2, 4, 6];
-productExceptSelf(nums);
+
+console.log(productExceptSelf(nums));
